3810eee5a637ed49c0828131f51e65baf1f56a2f
Nsilz44 <n_stbss@hotmail.com>
2023-10-18 00:39:37 +0100

    connected to database

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,5 +1,29 @@
+
+const [usernameReg, setUernameReg] = useState("");
+const [passwordReg, setPasswordReg] = useState ("");
+
 const SignIn = () => {
-    return <h1>Lgoin</h1>;
-  };
-  
+    return (
+        <div className="App">
+           <div className="registration">
+              <h1>Registration</h1>
+              <label>Username</label>
+              <input type="text" onChange={(e) => {
+              setUernameReg(e.target.value);
+           }}  /><br/>
+              <label>password</label>
+              <input type="text" onChange={(e) =>{
+              setPasswordReg(e.target.value);
+           }}  /> <br />
+              <button> Register</button>
+           </div>
+           <div className="login">
+              <h1>Login</h1>
+              <input type="text" placeholder="Username…" /> <br/>
+              <input type="password" placeholder="Password…" />
+              <button >Login</button>
+           </div>
+        </div>
+      );
+   };
   export default SignIn;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
+import db from "./database/db.js";
+import require from "./require.js";
+
 const express = require("express"); 
 const app = express(); 
+
   
 app.post("/post", (req, res) => { 
   console.log("Connected to React"); 
   res.redirect("/"); 
 }); 
+
+app.post("/register", (req, res)=> {
+  db.execute(
+    "INSERT INTO users (username, password) VALUES (?,?)",
+    [username, password],
+    (err, result)=> {
+    console.log(err);
+    }
+  );
+});
   
 const PORT = process.env.PORT || 8080; 
   
