61fa7e50a07ffc61c5bbab422ff3ebde178a8a35
Nsilz44 <n_stbss@hotmail.com>
2023-10-18 14:07:38 +0100

    registration basic details works

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,21 +1,51 @@
+import React ,{ useState } from "react";
 
-const [usernameReg, setUernameReg] = useState("");
-const [passwordReg, setPasswordReg] = useState ("");
+
+
 
 const SignIn = () => {
+   const [usernameReg, setUsernameReg] = useState("");
+   const [passwordReg, setPasswordReg] = useState ("");
+
+   const registerUser = () =>{ 
+      try {
+       const data = {
+          username : usernameReg,
+          password : passwordReg
+       }
+       console.log(data)
+       const res = fetch('http://localhost:8080/register', {
+         method: 'POST',
+         headers: {
+           "content-type": "application/json"
+         },
+         body: JSON.stringify(data),
+       });
+       console.log(res);
+    
+       if (!res.ok) console.log(`POST failed with ${res.status}.`);
+     } catch(err) {
+       console.error(err);
+     }
+   };
+
+   function RegUsernameChange(e) {
+      setUsernameReg(e.target.value);
+    };
+
+    function RegPasswordChange(e) {
+      setPasswordReg(e.target.value);
+    };
+   
     return (
         <div className="App">
            <div className="registration">
               <h1>Registration</h1>
               <label>Username</label>
-              <input type="text" onChange={(e) => {
-              setUernameReg(e.target.value);
-           }}  /><br/>
+              <input type="text" value={usernameReg} onChange={RegUsernameChange}   /><br/>
               <label>password</label>
-              <input type="text" onChange={(e) =>{
-              setPasswordReg(e.target.value);
-           }}  /> <br />
-              <button> Register</button>
+              <input type="text" value={passwordReg} onChange={RegPasswordChange}  /> <br />
+              <button onClick={registerUser} > Register</button>
            </div>
            <div className="login">
               <h1>Login</h1>
--- server/index.js
+++ server/index.js
@@ -3,14 +3,28 @@ import require from "./require.js";
 
 const express = require("express"); 
 const app = express(); 
+const cors = require("cors");
+const bodyParser = require('body-parser');
 
-  
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(
+  cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true,
+  })
+ );
+
 app.post("/post", (req, res) => { 
   console.log("Connected to React"); 
   res.redirect("/"); 
 }); 
 
 app.post("/register", (req, res)=> {
+  console.log(req.body);
+  const username = req.body.username;
+  const password = req.body.password;
   db.execute(
     "INSERT INTO users (username, password) VALUES (?,?)",
     [username, password],
