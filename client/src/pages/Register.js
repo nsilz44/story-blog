facb2e20d6d7f04f5d33f4868554288a89a2132b
Nsilz44 <n_stbss@hotmail.com>
2023-10-19 21:02:01 +0100

    passwords have to match before sending

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,10 +3,19 @@ import React ,{ useState } from "react";
 const Register = () => {
     const [usernameReg, setUsernameReg] = useState("");
     const [passwordReg, setPasswordReg] = useState ("");
+    const [confirmPasswordReg, setConfirmPasswordReg] = useState("");
     const [emailReg, setEmailReg] = useState("");
  
     const registerUser = () =>{ 
        try {
+        if(confirmPasswordReg !== passwordReg){
+            const matchingPassword = document.getElementById("matchingPasswords");
+            matchingPassword.style.display = "block";
+            console.log('now');
+        }
+        else {
+
+        
         const data = {
            username : usernameReg,
            password : passwordReg,
@@ -26,10 +35,11 @@ const Register = () => {
         if (!res.ok) {
           console.log(`POST failed with ${res.status}.`)
           console.log(res);};
-      } catch(err) {
+        }}catch(err) {
         console.error(err);
       };
-    };
+    
+    }
  
     function RegUsernameChange(e) {
        setUsernameReg(e.target.value);
@@ -38,6 +48,10 @@ const Register = () => {
      function RegPasswordChange(e) {
        setPasswordReg(e.target.value);
      };
+
+     function RegConfirmPasswordChange(e) {
+        setConfirmPasswordReg(e.target.value);
+      };
  
      function RegEmailChange(e) {
        setEmailReg(e.target.value);
@@ -46,14 +60,19 @@ const Register = () => {
      return (
          <div className="App">
             <div className="registration">
-               <h1>Registration</h1>
-               <label>Username</label>
-               <input type="text" value={usernameReg} onChange={RegUsernameChange}   /><br/>
-               <label>password</label>
-               <input type="text" value={passwordReg} onChange={RegPasswordChange}  /> <br />
-               <label>email</label>
-               <input type="text" value={emailReg} onChange={RegEmailChange}  /> <br />
-               <button onClick={registerUser} > Register</button>
+                <h1>Registration</h1>
+                <label>Username</label>
+                <input type="text" value={usernameReg} onChange={RegUsernameChange}   /><br/>
+                <label>Password</label>
+                <input type="text" value={passwordReg} onChange={RegPasswordChange}  /> <br />
+                <label>Retype password</label>
+                <input type="text" value={confirmPasswordReg} onChange={RegConfirmPasswordChange}  /> <br />
+                <div id="matchingPasswords" display = "none">
+                    <label>Passwords do not match</label>
+                </div>
+                <label>Email</label>
+                <input type="text" value={emailReg} onChange={RegEmailChange}  /> <br />
+                <button onClick={registerUser} > Register</button>
             </div>
          </div>
        );
